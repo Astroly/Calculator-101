7745ecf0a2497b35e5eb194670969a5871efd51b
zi <sirinya.hwanphet@gmail.com>
2020-01-05 23:58:48 +0700

    :x:  sass complie with  gulp does not work fixit!

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,16 @@ var gulp = require('gulp');
 gulp.task('taskName', function() {
     console.log('taskName');
 });
+
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+
+gulp.task('css', function(){
+  return sass('sass/')
+        .pipe(gulp.dest('./css'));
+});
+
+// gulp.task('default', ['sass'], function() {
+
+//     gulp.watch("scss/**/*.scss", ['sass']);
+// });
