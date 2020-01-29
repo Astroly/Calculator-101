d391d45476559f609807c42e738cc265cee7d093
Nitipon Numlamun <38374787+Astroly@users.noreply.github.com>
2020-01-29 15:24:25 +0700

    test maxlength

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
-function inp(value){
-  var num = document.getElementById("showNum").innerHTML += value;
+var num;
+var length =0;
+function numInput(value){
 
+
+  
+ var num = document.getElementById("showNum").innerHTML += value;
+  
+  //return num;
+  var n = num.length;
+  console.log(n);
 }
-function cal(){
-  let num1 = document.getElementById("showNum").innerHTML
-  let num2 = eval(num1)
-  document.getElementById("showNum").innerHTML = num2
- }
+
+// function addComma(num) {
+//   var parts = x.toString().split(".");
+//   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+//   return parts.join(".");
+// }
+
+
  function clr() 
 { 
    document.getElementById("showNum").innerHTML = ""
   
 } 
+
+
+
+
+
+
+// function numInput(value){
+//   var num = document.getElementById("showNum").innerHTML += value;
+
+// }
+// function cal(){
+//   let num1 = document.getElementById("showNum").innerHTML
+//   let num2 = eval(num1)
+//   document.getElementById("showNum").innerHTML = num2
+//  }
+//  function clr() 
+// { 
+//    document.getElementById("showNum").innerHTML = ""
+  
+// } 
